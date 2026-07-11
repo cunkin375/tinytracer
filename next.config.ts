1f8cc3dfe5205d2604c443d9012bc711c72fa9f6
Maxwell Lokshin <maxwell.lokshin@2zick.com>
2026-07-11 13:06:55 -0400

    html model editor

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      "*.wgsl": {
+        type: "raw",
+      },
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.wgsl$/,
